dab199e7d7ffbf4c9417aff010d463c69d71acea
James Gruber <jgrube01@calpoly.edu>
2024-03-02 18:17:15 -0800

    forgor to add src

---
+++ src/cache/cache_control.sv
@@ -0,0 +1,11 @@
+//Cache control for FSM & supporting logic. Braydon Burkhardt 2.27.24 rev A
+
+module cache_control (
+    input logic clk,
+    input logic rst
+    
+);
+
+
+
+endmodule : cache_control
