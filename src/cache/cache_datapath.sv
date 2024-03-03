33ca054a2fe63adcc957a8bcf8157d98169e270c
Braydon <31785964+draftdragon21@users.noreply.github.com>
2024-03-02 18:39:38 -0800

    added shadow_mem and added LRU i/o

--- src/cache/cache_datapath.sv
+++ src/cache/cache_datapath.sv
@@ -16,15 +16,18 @@ module cache_datapath #(
     input logic [255:0] dataIn,
     input logic [31:0] dataOut,
     
-    input logic [1:0] writeEn, //write enable for each way
+    input logic [1:0] dataWriteEn, //data write enable for each way
+    input logic [1:0] tagWriteEn, //tag write enable for each way
     input logic [1:0] setValid, //valid bit 1/0 to write each array to
     input logic [1:0] writeValid, //valid bit write enable for each array
     input logic [1:0] setDirty,
     input logic [1:0] writeDirty,
     
-    output logic [1:0] isHit, //1==hit, 0==miss
+    output logic isHit, //1==hit, 0==miss
     output logic [1:0] isValid, //1==valid, 0==nonvalid
-    output logic [1:0] isDirty //1==dirty, 0==clean
+    output logic [1:0] isDirty, //1==dirty, 0==clean
+    
+    output logic LRUout //gets the bit of the LRU array at the index; 0=LRU way A, 0=LRU way B
 );
 
 //todo: add needed reset logic
@@ -34,39 +37,48 @@ logic [23:0] memTag = memAddr[31:8];
 logic [2:0] memIndex = memAddr[7:5];
 logic [4:0] memOffset = memAddr[4:0];
 
-logic read;
-logic write_en [31:0];
 logic [2:0] rindex; //read index
 logic [2:0] windex; //write index
+
+//Data array stuff
+logic dataread;
+logic write_en_A [31:0]; //32 bytes long and we want to be able to be byte addressable to the CPU
+logic write_en_B [31:0];
 logic [255:0] datain;
-logic [255:0] dataout;
+logic [255:0] dataout_A;
+logic [255:0] dataout_B;
 
 data_array #(.s_offset(5), .s_index(3)) DataArrayA (
     .clk(clk),
     .rst(rst),
-    .read(read),
-    .write_en(write_en),
+    .read(dataread),
+    .write_en(write_en_A),
     .rindex(rindex),
     .windex(windex),
     .datain(datain),
-    .dataout(dataout)
+    .dataout(dataout_A)
   );
 data_array #(.s_offset(5), .s_index(3)) DataArrayB (
     .clk(clk),
     .rst(rst),
-    .read(read),
-    .write_en(write_en),
+    .read(dataread),
+    .write_en(write_en_B),
     .rindex(rindex),
     .windex(windex),
     .datain(datain),
-    .dataout(dataout)
+    .dataout(dataout_B)
   );
+
+//tag array stuff
+logic tagread;
+logic tagload_A, tagload_B;
+
 
 array #(.s_index(3), .s_width(24)) TagArray_A (
     .clk(clk),
     .rst(rst),
-    .read(read),
-    .load(load),
+    .read(tagread),
+    .load(tagload_A),
     .rindex(rindex),
     .windex(windex),
     .datain(datain),
@@ -75,8 +87,8 @@ array #(.s_index(3), .s_width(24)) TagArray_A (
 array #(.s_index(3), .s_width(24)) TagArray_B (
     .clk(clk),
     .rst(rst),
-    .read(read),
-    .load(load),
+    .read(tagread),
+    .load(tagload_B),
     .rindex(rindex),
     .windex(windex),
     .datain(datain),
@@ -123,6 +135,17 @@ array #(.s_index(3), .s_width(1)) dirtyArray_B (
     .windex(windex),
     .datain(datain),
     .dataout(dataout)
-  ); 
+  );
+  
+array #(.s_index(3), .s_width(1)) LRUArray (
+    .clk(clk),
+    .rst(rst),
+    .read(read),
+    .load(load),
+    .rindex(rindex),
+    .windex(windex),
+    .datain(datain),
+    .dataout(dataout)
+  );
 
 endmodule : cache_datapath
